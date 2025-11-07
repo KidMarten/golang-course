629b7ca5ab07d53611bc32562191ebdd40aa438a
KidMarten <kunitsynpv@ya.ru>
2025-11-07 21:08:04 +0300

    added get user info function

--- imt/main.go
+++ imt/main.go
@@ -6,14 +6,7 @@ import (
 )
 
 func main() {
-	var height float64
-	var weight float64
-
-	fmt.Print("Enter your height (sm): ")
-	fmt.Scan(&height)
-	fmt.Print("Enter your weight: ")
-	fmt.Scan(&weight)
-
+	height, weight := getUserInput()
 	IMT := calculateIMT(height, weight)
 	outputResult(IMT)
 }
@@ -27,4 +20,16 @@ func calculateIMT(height float64, weight float64) float64 {
 	const IMTPower = 2
 	IMT := weight / math.Pow(height/100, IMTPower)
 	return IMT
+}
+
+func getUserInput() (float64, float64) {
+	var height float64
+	var weight float64
+
+	fmt.Print("Enter your height (sm): ")
+	fmt.Scan(&height)
+	fmt.Print("Enter your weight: ")
+	fmt.Scan(&weight)
+
+	return height, weight
 }

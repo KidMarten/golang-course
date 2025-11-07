e8020fdf4be6eb247e78f3bdaf0c24b44675bc1b
KidMarten <kunitsynpv@ya.ru>
2025-11-07 20:19:09 +0300

    pi calculation script

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func roundToTwoDecimalPlaces(f float64) float64 {
+	return math.Round(f*100) / 100
+}
 
 func main() {
-	fmt.Printf("Hello")
+	radiusStr := os.Args[1]
+	radius, err := strconv.ParseFloat(radiusStr, 64)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if radius < 0.1 || radius > 100.0 {
+		fmt.Printf("Error. Radius must be between 0.1 and 100")
+		os.Exit(1)
+	}
+
+	var area = math.Pi * math.Pow(radius, 2)
+	var formattedArea = roundToTwoDecimalPlaces(area)
+	fmt.Print(formattedArea)
 }

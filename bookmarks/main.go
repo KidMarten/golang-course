fc52c7990d2492bc5aca42b5cabab645199bfe9f
KidMarten <kunitsynpv@ya.ru>
2025-12-06 22:42:42 +0300

    feat: added map alias and labels app for bookmarks

---
+++ bookmarks/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+)
+
+type bookmarkMap = map[string]string
+
+func main() {
+	bookmarks := bookmarkMap{}
+Menu:
+	for {
+		input := getMenu()
+		switch input {
+		case 1:
+			printBookmarks(bookmarks)
+		case 2:
+			bookmarks = addBookmark(bookmarks)
+		case 3:
+			bookmarks = deleteBookmark(bookmarks)
+		case 4:
+			break Menu
+		}
+	}
+}
+
+func getMenu() int {
+	var input int
+	fmt.Println("1. List bookmarks")
+	fmt.Println("2. Add")
+	fmt.Println("3. Delete")
+	fmt.Println("4. Exit")
+	fmt.Scan(&input)
+	return input
+}
+
+func printBookmarks(bookmarks bookmarkMap) {
+	if len(bookmarks) == 0 {
+		fmt.Println("No bookmarks yet")
+	}
+	for key, value := range bookmarks {
+		fmt.Println(key, ":", value)
+	}
+}
+
+func addBookmark(bookmarks bookmarkMap) bookmarkMap {
+	var newBookmarkKey string
+	var newBookmarkValue string
+
+	fmt.Print("Enter name: ")
+	fmt.Scan(&newBookmarkKey)
+	fmt.Print("Enter value: ")
+	fmt.Scan(&newBookmarkValue)
+
+	bookmarks[newBookmarkKey] = newBookmarkValue
+	return bookmarks
+}
+
+func deleteBookmark(bookmarks bookmarkMap) bookmarkMap {
+	var bookmarkToDelete string
+	fmt.Print("Enter name to delete: ")
+	fmt.Scan(&bookmarkToDelete)
+	delete(bookmarks, bookmarkToDelete)
+	return bookmarks
+}

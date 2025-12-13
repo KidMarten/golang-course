4427110c1dd2b00010498d8ecf82d0bdc283656e
KidMarten <kunitsynpv@ya.ru>
2025-12-13 18:41:31 +0300

    added defer to write file

--- passwords/files/files.go
+++ passwords/files/files.go
@@ -15,11 +15,10 @@ func WriteFile(content string, name string) {
 		fmt.Println(err)
 	}
 	_, err = file.WriteString(content)
+	defer file.Close()
 	if err != nil {
-		file.Close()
 		fmt.Println(err)
 		return
 	}
 	fmt.Println("Wirte successful")
-	file.Close()
 }

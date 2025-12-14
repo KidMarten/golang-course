91678f96b8a58e4268a12c6bfcb6de6082132194
KidMarten <kunitsynpv@ya.ru>
2025-12-14 18:30:45 +0300

    added file reading to bin project

--- 3-bin/file/file.go
+++ 3-bin/file/file.go
@@ -1,1 +1,26 @@
 package file
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func isJSONFile(filename string) bool {
+	ext := filepath.Ext(filename)
+	return strings.ToLower(ext) == ".json"
+}
+
+func ReadFile(name string) ([]byte, error) {
+	if isJSONFile(name) {
+		data, err := os.ReadFile(name)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		return data, nil
+	} else {
+		return nil, fmt.Errorf("Not a json file")
+	}
+}
